13d70dc08f66643e2b3adfb222dc12a81ac5d879
Gianluca Mondini <mondini@open2b.com>
2025-11-06 12:32:19 +0100

    Reformat file `setup.py`

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ try:
 except ImportError:
     from distutils.core import setup
 # Don't import analytics-python module here, since deps may not be installed
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'meergo','analytics'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "meergo", "analytics"))
 from version import VERSION
 
 install_requires = [
     "requests~=2.7",
     "backoff~=2.1",
     "python-dateutil~=2.2",
-    "PyJWT~=2.8.0"
+    "PyJWT~=2.8.0",
 ]
 
 tests_require = [
@@ -23,13 +23,11 @@ tests_require = [
 ]
 
 setup(
-    name='meergo-analytics-python',
+    name="meergo-analytics-python",
     version=VERSION,
-    packages=['meergo.analytics', 'meergo.analytics.test'],
-    python_requires='>=3.9.0',
-    license='MIT License',
+    packages=["meergo.analytics", "meergo.analytics.test"],
+    python_requires=">=3.9.0",
+    license="MIT License",
     install_requires=install_requires,
-    extras_require={
-        'test': tests_require
-    },
+    extras_require={"test": tests_require},
 )
